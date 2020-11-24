f69f82a466dabcf9c34cab61c372e5a5a4ba6edd
yjc <>
2020-11-24 14:37:01 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React,{Component} from 'react';//引入react核心库
-import {Route,Switch} from 'react-router-dom'
-import './App.less';//react脚手架中引入样式的方式
-import Login from './page/login/login.jsx';
-import Admin from './page/admin/admin.jsx';
-//通过关键字对作者进行axios请求查找对应的作者并展示作者的头像和名字
+import React,{Component} from 'react'
+
 export default class App extends Component{
   render(){
     return (
-      <div className="app">
-        <Switch>
-            <Route path="/login" component={Login}></Route>
-            <Route path="/admin" component={Admin}></Route>
-        </Switch>
-
-      </div>
+        <div>App</div>
     )
   }
 }
-
-
--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom'
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+
+ReactDom.render(<App/>,document.getElementById('root'))
