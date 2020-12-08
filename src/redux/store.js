7117787270500d9d2a327570fb4ee668fe126dd4
yjc <>
2020-12-08 14:37:00 +0800

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,7 @@ import {createStore,applyMiddleware} from 'redux'
 import reducers from './reducers'
 
 //引入redux-thunk
-import thunk from 'react-thunk'
+import thunk from 'redux-thunk'
 
 //引入redux-devtools-extension，用于支持redux开发工具
 import {composeWithDevTools} from 'redux-devtools-extension'
